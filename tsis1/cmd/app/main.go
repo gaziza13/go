217cc79faf39f5a3f869cccc8a392ca19613115e
Gaziza Akhmetzhan <gaziza@MacBook-Air-Gaziza.local>
2024-02-02 17:12:05 +0600

    Added copyright statement with email

---
+++ tsis1/cmd/app/main.go
@@ -0,0 +1,20 @@
+// main.go (inside cmd/mygoapp/)
+package main
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+	"github.com/gaziza13/go/api"
+)
+
+func main() {
+	r := mux.NewRouter()
+
+	r.HandleFunc("/music", api.GetAllMusics).Methods("GET")
+	r.HandleFunc("/music/{id}", api.GetMusic).Methods("GET")
+	r.HandleFunc("/health", api.HealthCheck).Methods("GET")
+
+	log.Fatal(http.ListenAndServe(":80", r))
+}
